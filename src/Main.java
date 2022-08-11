fd05b858827111472aa659f92e2dc108e4953a49
Andrew Lazarenkov <lazarenkovandrew@gmail.com>
2022-08-11 23:57:02 +0300

    fix: project recreated

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 100; //первоначальный баланс
+        int payment = 1025; //сумма платежа
+        int bonus;
+        if (payment >= 1000) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println("Сумма платежа - " + payment + " рублей.");
+        System.out.println("Текущий баланс - " + (amount + payment) + " рублей.");
+
+        if (bonus > 0) {
+            System.out.println("Начислено " + (bonus) + " бонусов.");
+        } else {
+            System.out.println("Бонусы не начислены, пополнение менее чем на 1000 руб.");
+        }
+    }
+
+}
